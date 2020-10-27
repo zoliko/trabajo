5d3beb67c99cc3efb5ff6035a248b693f1ba1f1b
Mushu_Marce <waxelo77@gmail.com>
2020-10-27 13:48:21 -0600

    Initial Commit

---
+++ Ramirez Soto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ramirez Soto
+//
+//  Created by Mushu-Marcelo on 27/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
